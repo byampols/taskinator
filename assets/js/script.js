13bc109f4ee6fbf1eb7a7aa7bd0710d81a3d5cfb
unknown <byampols@alumni.cmu.edu>
2021-08-23 17:11:13 -0400

    allowed submitted text/type to appear in the list elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,20 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var createTaskHandler = function() {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+
+    //create a div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info";
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemE1.appendChild(taskInfoE1);
+
+    //add entire item to list
     tasksToDoE1.appendChild(listItemE1);
 }
 
